ce2f95ef257e735a7938e795884cac2db8242898
Robert De Matteo <robertdematteo@hotmail.com>
2019-03-05 16:44:31 +1100

    Table output

--- scheduler.rb
+++ scheduler.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'terminal-table'
+
 # Welcome message
 system "clear"
 puts "Welcome to the Program Schedular.\nThis app will allow the user to set name and title for multpile speakers in multiple sessions.\nThere are 2 sessions, each with four(4) time slots.\n\n"
@@ -38,5 +39,26 @@ end
 
 
 
+#Table output
+def output_table
+rows = []
+rows << ['One', '10:00', 'A', 'A']
+rows << ['Two', '10:30', 'B','A']
+rows << ['Three', '11:00', 'C','A']
+rows << ['Four', '11:30', 'D','A']
+table1 = Terminal::Table.new :title => "Session 1", :headings => ['Speaker', 'Time', 'Name', 'Title'], :rows => rows
+puts
+rows = []
+rows << ['One', '1:00', 'A', 'A']
+rows << ['Two', '1:30', 'B','A']
+rows << ['Three', '2:00', 'C','A']
+rows << ['Four', '2:30', 'D','A']
+table2 = Terminal::Table.new :title => "Session 2", :headings => ['Speaker', 'Time', 'Name', 'Title'], :rows => rows
 
+puts table1
+puts
+puts table2
 
+end
+
+output_table
