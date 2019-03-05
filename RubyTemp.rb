5df73414dc681330c2bb2f94e1c5d5f681a0d974
root <root@DESKTOP-DFNAB83.localdomain>
2019-03-06 04:01:32 +1100

    save to master

--- RubyTemp.rb
+++ RubyTemp.rb
@@ -1,7 +1,107 @@
-h2 = {}
-h2["one"] = "北京"
-h2["two"] = "上海"
-h2["three"] = "深圳"
-h2["four"] = "广州"
+Session_1 = {}
+Session_2 = {}
 
-puts h2
+Session_1[:"speaker 1"] = {}
+Session_1[:"speaker 2"] = {}
+Session_1[:"speaker 3"] = {}
+Session_1[:"speaker 4"] = {}
+Session_2[:"speaker 1"] = {}
+Session_2[:"speaker 2"] = {}
+Session_2[:"speaker 3"] = {}
+Session_2[:"speaker 4"] = {}
+
+
+
+
+
+def Option()
+    puts ("Select an option from 1-3 (3 to quit)")
+    option = gets().strip
+# puts table 1
+    case option
+# require 'terminal-table'
+    when "1"                 # Table Options
+        puts ("Now it's time to input your Speech Detail")
+        puts ("Please enter your session number")
+        session_no = gets().strip
+        puts ("Please enter your speaker number")
+        speaker_No = gets().strip
+        add_detail_to_backlog()
+        #  show table 2
+        # puts table 2
+        puts ("Now please enter your name")
+        name = gets().strip
+        puts ("Now please enter your title")
+        title = gets().strip
+        # table 3
+
+        return speaker_No
+
+    # # new_title[:title] = title
+        #puts ("Now please choose your session No")
+    # Session_No = gets().strip
+
+    #
+    when "2"
+        puts ("Now you chose otpion 2")
+        puts ("Now you chose to ouput information")
+        puts ("Please Choose from the following options (1 or 2)")
+        output = gets().strip
+        if output == "table"
+        puts "output table"
+        elsif output == "name and title"
+        # puts "name and title table"
+        end
+    when "3"
+        puts ("You chose otpion 3")
+        puts ("Now you chose to quit")
+        puts("=====================Exited==========================")
+    else
+        puts("Error. Invalid option.")
+    end
+end
+
+
+
+
+def add_detail_to_backlog()
+    new_person[:name] = name
+    new_person[:title] = title
+
+    while
+        case speaker_No
+        new_person[:name] = name
+        new_person[:title] = title
+        if  session_no = 1
+            when 1
+                Session_1[:"speaker 1"] << new_person
+            when 2
+                Session_1[:"speaker 2"] << new_person
+            when 3
+                Session_1[:"speaker 3"] << new_person
+            when 4
+                Session_1[:"speaker 4"] << new_person
+            else
+                puts ("Wrong Number")
+            end
+        end
+
+        if  session_no = 2
+            when 1
+                Session_1[:"speaker 1"] << new_person
+            when 2
+                Session_1[:"speaker 2"] << new_person
+            when 3
+                Session_1[:"speaker 3"] << new_person
+            when 4
+                Session_1[:"speaker 4"] << new_person
+            else
+                puts ("Wrong Number")
+            end
+        end
+    end
+end
+
+
+
+Option()
--- scheduler.rb
+++ scheduler.rb
@@ -1,100 +1,131 @@
 
-puts ("Select an option from 1-3 (3 to quit)")
-option = gets().strip
-# puts table 1
+
+session_1 = {}
+session_2 = {}
+
+session_1[:"speaker 1"] = {}
+session_1[:"speaker 2"] = {}
+session_1[:"speaker 3"] = {}
+session_1[:"speaker 4"] = {}
+session_2[:"speaker 1"] = {}
+session_2[:"speaker 2"] = {}
+session_2[:"speaker 3"] = {}
+session_2[:"speaker 4"] = {}
+
+
 def Option()
-
-
-Speach_Detail = {}
-
-Session_1 = {}
-Session 2 = {}
-
-Session_1[:"Speaker 1"] = ""
-Session_1[:"Speaker 2"] = ""
-Session_1[:"Speaker 3"] = ""
-Session_1[:"Speaker 4"] = ""
-Session_2[:"Speaker 1"] = ""
-Session_2[:"Speaker 2"] = ""
-Session_2[:"Speaker 3"] = ""
-Session_2[:"Speaker 4"] = ""
-
-
-case option
+    puts ("Select an option from 1-3 (3 to quit)")
+    option = gets().strip
+# puts table 1
+    case option
 # require 'terminal-table'
-when "1"                 # Table Options
-    puts ("Now it's time to input your Speech Detail")
-    puts ("Now please choose your session No")  #  show table 2
-    # puts table 2
-    Session_No = gets().strip
-    # Speach_Detail[:Session] = Session_No
-
-    puts ("Please enter your speaker No") # table 3
-    # puts table 3
-    Speaker_No = gets().strip
-    Speach_Detail[:Speaker] = Speaker_No
-    puts ("Now please enter your name")
-    Name = gets().strip
-    # new_person[:name] = Name
-    puts ("Now please enter your title")
-    Title = gets().strip
-    Speach_Detail[:Title] = Title
-    # new_title[:title] = Title
-
-when "2"
-    puts ("Now you chose otpion 2")
-    puts ("Now you chose to ouput information")
-    puts ("Please Choose from the following options (1 or 2)")
-    output = gets().strip
-    if output == "table"
-        # puts output table
-    if output == "name and title"
-        # puts name and title table
-
-when "3"
-  puts ("You chose otpion 3")
-  puts ("Now you chose to quit")
-  puts"(quited)
+    when "1"                 # Table Options
+        puts ("Now it's time to input your Speech Detail")
+        puts ("Please enter your session number")
+        session_no = gets().strip
 
 
-else
-  puts("Error. Invalid option.")
-end
+        puts ("Please enter your speaker number")
+        speaker_No = gets().strip
+
+        #  show table 2
+        # puts table 2
+        puts ("Now please enter your name")
+        name = gets().strip
+        puts name
+        puts ("Now please enter your title")
+        title = gets().strip
+        puts title
+        new_person = {}
+        new_person[:name] = name
+        new_person[:title] = title
+        add_detail_to_backlog()
+        # table 3
 
 
 
 
-def add_detail_to_backlog
-    new_person[:name] = Name
-    new_person[:title] = Title
-    while
-        case Speaker_No
-        when 1
-            Speaker 1 << new_person
-        when 2
-            Speaker 2 << new_person
-        when 3
-            Speaker 3 << new_person
-        when 4
-            Speaker 3 << new_person
-        else
-            puts ("Wrong Number")
+    # # new_title[:title] = title
+        #puts ("Now please choose your session No")
+    # session_No = gets().strip
+
+    #
+    when "2"
+        puts ("Now you chose otpion 2")
+        puts ("Now you chose to ouput information")
+        puts ("Please Choose from the following options (1 or 2)")
+        output = gets().strip
+        if output == "table"
+        puts "output table"
+        elsif output == "name and title"
+        puts "name and title table"
         end
+
+    when "3"
+        puts ("You chose otpion 3")
+        puts ("Now you chose to quit")
+        puts("=====================Exited==========================")
+    else
+        puts("Error. Invalid option.")
     end
 end
 
 
 
 
-
-while run_program == "yes"
-
-    new_person = {}
+def add_detail_to_backlog()
 
 
-    add_detail_to_backlog(new_session,new_speaker, new_name, new_title)
 
-    puts "Would you like to make another order - yes or no?"
-    run_program = gets.strip().downcase
+    while
+
+        if  session_no == 1
+            case speaker_No
+            when 1
+                session_1[:"speaker 1"] << new_person
+            when 2
+                session_1[:"speaker 2"] << new_person
+            when 3
+                session_1[:"speaker 3"] << new_person
+            when 4
+                session_1[:"speaker 4"] << new_person
+            else
+                puts ("Wrong Number")
+            end
+        end
+
+        if  session_no == 2
+            case speaker_No
+            when 1
+                session_1[:"speaker 1"] << new_person
+            when 2
+                session_1[:"speaker 2"] << new_person
+            when 3
+                session_1[:"speaker 3"] << new_person
+            when 4
+                session_1[:"speaker 4"] << new_person
+            else
+                puts ("Wrong Number")
+            end
+        end
+    end
 end
+
+
+Option()
+
+puts session_1
+puts session_2
+
+
+# while run_program == "yes"
+
+#     new_person = {}
+
+
+#     add_detail_to_backlog(new_session,new_speaker, new_name, new_title)
+
+#     puts "Would you like to make another order - yes or no?"
+#     run_program = gets.strip().downcase
+# end
 
